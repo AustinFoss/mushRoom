0febc397c9a6225312fdee8e607b8ffdfe4f0113
eru <morris.austin.t@gmail.com>
2018-11-27 20:53:18 -0700

    restructured arduino logic & add the ability to receive instructions

--- mushRoom.ino
+++ mushRoom.ino
@@ -10,24 +10,29 @@ const byte relay2 = A1;
 const byte relay3 = A2;
 const byte relay4 = A3;
 
+bool looping = true;
 byte roomID = 1;
 byte envProfile = 0;
 
-byte maxTmp = 10;
-byte minHum = 90;
+byte maxTmp = 25;
+byte minHum = 40;
+byte maxCO2 = 255;
+bool lights = false;
+
 
 // The IP address will be determined by a dhcp
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 //Listening for a specific client's IP, security measure
-IPAddress listeningFor(172, 16, 89, 189);
+//IPAddress listeningFor(172, 16, 89, 189); //warehouse IP
+IPAddress listeningFor(172, 16, 89, 189); //home IP
 
 unsigned int localPort = 8888;   // local port to listen on
 unsigned int endPort = 8888;     // port to send data to
 
 // buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet
 char batchId[] = "IO.2018.11.20";
 
 //Create Ethernet UDP instance
@@ -35,11 +40,11 @@ EthernetUDP Udp;
 
 void setup() {
 
-  // Serial Setup
-  // Serial.begin(9600);
-  // while (!Serial) {
-  //   ; // wait for serial port to connect. Needed for native USB port only
-  // }
+//   Serial Setup
+  Serial.begin(9600);
+  while (!Serial) {
+   ; // wait for serial port to connect. Needed for native USB port only
+  }
 
   pinMode(relay1, INPUT_PULLUP); // first enable pull up, to stop relay chatter during bootup
   pinMode(relay1, OUTPUT); // then set pin to output
@@ -54,13 +59,13 @@ void setup() {
   Ethernet.init(10);
   Ethernet.begin(mac);
 
-  // Serial.println("Initialize Ethernet with DHCP:");
+  Serial.println("Initialize Ethernet with DHCP:");
   if (Ethernet.begin(mac) == 0) {
-    // Serial.println("Failed to configure Ethernet using DHCP");
+    Serial.println("Failed to configure Ethernet using DHCP");
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      // Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
     } else if (Ethernet.linkStatus() == LinkOFF) {
-      // Serial.println("Ethernet cable is not connected.");
+      Serial.println("Ethernet cable is not connected.");
     }
     // no point in carrying on, so do nothing forevermore:
     while (true) {
@@ -68,8 +73,8 @@ void setup() {
     }
   }
   // print your local IP address:
-  // Serial.print("My IP address: ");
-  // Serial.println(Ethernet.localIP());
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
 
   // start UDP
   Udp.begin(localPort);
@@ -85,91 +90,116 @@ void setup() {
 
 void loop() {
 
-    byte humd = sht20.readHumidity();                  // Read Humidity
-    byte temp = sht20.readTemperature();               // Read Temperature
-    byte co2 = 255;
+  byte humd = sht20.readHumidity();                  // Read Humidity
+  byte temp = sht20.readTemperature();               // Read Temperature
+  byte co2 = 255;
 
-    byte isRelay1 = digitalRead(relay1);
-    byte isRelay2 = digitalRead(relay2);
-    byte isRelay3 = digitalRead(relay3);
-    byte isRelay4 = digitalRead(relay4);
+  byte isRelay1 = digitalRead(relay1);
+  byte isRelay2 = digitalRead(relay2);
+  byte isRelay3 = digitalRead(relay3);
+  byte isRelay4 = digitalRead(relay4);
 
-    if (temp >= maxTmp) {
-      if (digitalRead(relay1) == 1) {
-        digitalWrite(relay1, LOW); // relay 'on'
-      }
-    } else {
-      if (digitalRead(relay1) == 0) {
-        digitalWrite(relay1, HIGH); // relay 'off'
-      }
-    }
-
-    if (humd <= minHum) {
-      if (digitalRead(relay2) == 1) {
-        digitalWrite(relay2, LOW); // relay 'on'
-      }
-    } else {
-      if (digitalRead(relay2) == 0) {
-        digitalWrite(relay2, HIGH); // relay 'on'
-      }
-    }
-
-//    Serial.println();
-//    Serial.println("Temperature: ");
-//    Serial.print(temp);
-//    Serial.println();
-//    Serial.println("Humidity: ");
-//    Serial.print(humd);
-
-//if there's data available, read a packet
   int packetSize = Udp.parsePacket();
+
+  // Packet Logic
   if (packetSize) {
     if (Udp.remoteIP() == listeningFor) {
+
+      IPAddress remote = Udp.remoteIP();
+      // read the packet into packetBufffer
+      Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+
+      // Update listeningFor IP
+      if (packetBuffer[0] == 1) {
+        Serial.println("Update listeningFor IP");
+      }
+
+      // Update Profile
+      if (packetBuffer[0] == 2) {
+        Serial.println();
+        Serial.println("Profile Updated");
+        Serial.println();
+        envProfile = packetBuffer[1];
+        maxTmp = packetBuffer[2];
+        minHum = packetBuffer[3];
+        maxCO2 = packetBuffer[4];
+        if (packetBuffer[5] == 0) {
+          lights = true;
+        }
+        if (packetBuffer[5] == 1) {
+          lights = false;
+        }
+
+        // Serial.print("Environment Profile ID: ");
+        // Serial.print(envProfile);
         // Serial.println();
-        // Serial.print("Received packet of size: ");
-        // Serial.print(packetSize);
+        // Serial.print("Max Temp: ");
+        // Serial.print(maxTmp);
         // Serial.println();
-        // Serial.print("From: ");
-        IPAddress remote = Udp.remoteIP();
-        // for (int i=0; i < 4; i++) {
-        //   Serial.print(remote[i], DEC);
-        //   if (i < 3) {
-        //     Serial.print(".");
-        //   }
+        // Serial.print("Min Hum: ");
+        // Serial.print(minHum);
+        // Serial.println();
+        // Serial.print("Max CO2: ");
+        // Serial.print(maxCO2);
+        // Serial.println();
+        // if (digitalRead(relay4) == 0) {
+        //   Serial.print("Lights: On");
         // }
-        // Serial.print(", port: ");
-        // Serial.print(Udp.remotePort());
-
-        // read the packet into packetBufffer
-        Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+        // if (digitalRead(relay4) == 1) {
+        //   Serial.print("Lights: Off");
+        // }
         // Serial.println();
-        // Serial.print("Contents: ");
-        // Serial.print(packetBuffer);
+      }
 
-
-        // Serial.println();
-        // Serial.println("Temperature: ");
-        // Serial.print(temp);
-        // Serial.println();
-        // Serial.println("Humidity: ");
-        // Serial.print(humd);
+      // Send Data
+      if (packetBuffer[0] == 3) {
         byte replyBuffer[] = {roomID, envProfile, temp, humd, co2, isRelay1, isRelay2, isRelay3, isRelay4};
-
-        // send a reply to the IP address and port that sent us the packet we received
         Udp.beginPacket(listeningFor, endPort);
         Udp.write(batchId);
         for (int i=0; i<9; i++){
           Udp.write(replyBuffer[i]);
         };
         Udp.endPacket();
-        // Serial.println();
-        // Serial.println("Data Sent");
-        // Serial.println();
+        Serial.println("Send Data");
       }
-    // else {
-    //   Serial.print("Received Packet From Unknown Source ");
-    //   Serial.print(Udp.remoteIP());
-    //   }
+
+    } else {
+      Serial.print("Received Packet From Unknown Source ");
+      Serial.print(Udp.remoteIP());
     }
+  }
+
+  // Environment Logic
+  if (temp >= maxTmp) {
+    if (digitalRead(relay1) == 1) {
+      digitalWrite(relay1, LOW); // relay 'on'
+    }
+  } else {
+    if (digitalRead(relay1) == 0) {
+      digitalWrite(relay1, HIGH); // relay 'off'
+    }
+  }
+
+  if (humd <= minHum) {
+    if (digitalRead(relay2) == 1) {
+      digitalWrite(relay2, LOW); // relay 'on'
+    }
+  } else {
+    if (digitalRead(relay2) == 0) {
+      digitalWrite(relay2, HIGH); // relay 'on'
+    }
+  }
+
+  if (lights == true) {
+    if (digitalRead(relay4) == 1) {
+      digitalWrite(relay4, LOW); // relay 'on'
+    }
+  } else {
+    if (digitalRead(relay4) == 0) {
+      digitalWrite(relay4, HIGH); // raley 'off'
+    }
+  }
+
   delay(10);
+
 }
